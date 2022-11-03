cd0f78787b49001459715c3c6bd9fa33725b416f
Thomas-mh <thomas.dumont@edu.ece.fr>
2022-11-03 18:16:03 -0500

    feat(dermatologists): updated login form dermatologist.

--- src/app/public/security/login-form-dermatologist/login-form-dermatologist.component.ts
+++ src/app/public/security/login-form-dermatologist/login-form-dermatologist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../../../overview/services/user.service";
 
 @Component({
   selector: 'app-login-form-dermatologist',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form-dermatologist.component.css']
 })
 export class LoginFormDermatologistComponent implements OnInit {
-
-  constructor() { }
+  dataDermatologist: any = {
+    name: "",
+    lastName: "",
+    age: 18,
+    address: "",
+    email: "",
+    password: "",
+    urlImage: ""
+  }
+  next = true;
+  constructor(private router: Router, private patientService: UserService) { }
 
   ngOnInit(): void {
   }
-
+  Login(formTemplate: any){
+    if(formTemplate.hasError('required') ){
+      console.log("algo salio mal")
+    }else{
+      this.patientService.getDermatologists().subscribe((data)=>{
+        console.log(this.dataDermatologist.email)
+        data.map((e)=>{
+          if(e.email === this.dataDermatologist?.email && e.password === this.dataDermatologist?.password){
+            this.router.navigate(['patients']).then();
+            localStorage.setItem("dermatologist", JSON.stringify(e));
+          }
+        })
+      })
+    }
+  }
 }
