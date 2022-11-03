4810afce66cda446f272dded88365c8f819d61cc
Thomas-mh <thomas.dumont@edu.ece.fr>
2022-11-03 18:24:29 -0500

    feat(patients): updated login form patient.

--- src/app/public/security/login-form-patient/login-form-patient.component.ts
+++ src/app/public/security/login-form-patient/login-form-patient.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../../../overview/services/user.service";
+
 
 @Component({
   selector: 'app-login-form-patient',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form-patient.component.css']
 })
 export class LoginFormPatientComponent implements OnInit {
+  dataPatient: any = {
+    name: "",
+    lastName: "",
+    age: 18,
+    address: "",
+    email: "",
+    password: "",
+    urlImage: ""
+  }
+  next = true;
 
-  constructor() { }
+  constructor(private router: Router, private patientService: UserService) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  Login(formTemplate: any){
+    if(formTemplate.hasError('required') ){
+      console.log("algo salio mal")
+    }else{
+      this.patientService.getPatients().subscribe((data)=>{
+        data.map((e)=>{
+          if(e.email === this.dataPatient?.email && e.password === this.dataPatient?.password){
+            this.router.navigate(['home-patient']).then();
+            localStorage.setItem("patient", JSON.stringify(e));
+          }
+        })
+      })
+    }
+  }
 }
