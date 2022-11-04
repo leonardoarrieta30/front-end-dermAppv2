e93694aac1ab4550fc8991945c5e14435a3aa238
DiegoDeLaFlor <u202017237@upc.edu.pe>
2022-11-04 11:47:15 -0500

    fix(register): update register form and app routing.

---
+++ src/app/public/security/register-form/register-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  users=[
+    {
+      value:'Patient',
+      viewValue:'patient '
+
+    },
+    {
+      value:'Dermatologist',
+      viewValue:'Dermatologist'
+    }
+
+  ]
+}
